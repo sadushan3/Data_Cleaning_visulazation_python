9ed545bf932e8320320dbe8c6e8fff828f73c5f6
Sadurshan <167612987+babyele@users.noreply.github.com>
2025-05-25 22:50:43 +0530

    added visualizations

--- Pthon_cleaning.py
+++ Pthon_cleaning.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #cleaning the data
 data_set=pd.read_csv("data.csv",na_values=[""," ","  ","\t","\xa0"])
@@ -26,3 +27,20 @@ new_data_set=pd.read_csv("cleaned_data_file.csv")
 
 #No duplicate values in the data set
 print(new_data_set.duplicated().sum())
+
+
+new_data_set["Viewership_Millions"].plot(kind='hist',bins=20,title='Viwership in millions comparation')
+plt.xlabel="Viewership_Millions"
+plt.tight_layout()
+plt.show()
+
+new_data_set.plot.scatter(x="Viewership_Millions",y="Title")
+plt.xlabel="Viewership_Millions"
+plt.tight_layout()
+plt.show()
+
+new_data_set['Genre'].value_counts().head(5).plot(kind="bar",title="first five generes")
+plt.xlabel="Genre"
+plt.ylabel("Count")
+plt.tight_layout()
+plt.show()
